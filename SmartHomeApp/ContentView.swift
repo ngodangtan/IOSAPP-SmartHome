e057d05d258fbbe70cb97f88bfe797610b4bd07b
Tan Ngo Dang <ngodangtan1994@gmail.com>
2025-06-03 20:12:11 +0700

    Initial Commit

---
+++ SmartHomeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartHomeApp
+//
+//  Created by Tan Ngo Dang on 3/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartHomeApp/SmartHomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeAppApp.swift
+//  SmartHomeApp
+//
+//  Created by Tan Ngo Dang on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
